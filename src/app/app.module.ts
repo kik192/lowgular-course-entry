b30f917ab171a0e25e5aeab3f77731a7771e0a38
Programowanie <k.kacka5@gmail.com>
2022-11-18 10:33:36 +0100

    feat:add faces module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
+import {FacesComponentModule} from "./ui/faces/faces.component-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +12,7 @@ import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.co
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FacesComponentModule,
     EmployeeListComponentModule,
   ],
   providers: [],
